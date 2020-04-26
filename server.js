19f5e387ad170d34923516c5c9c9ce71ffdfe3ce
PiotrGrobelak <grobelak.piotr@gmail.com>
2020-04-26 21:22:06 +0200

    feat: install dependencies and run server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const PORT = 3000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on Port: " + PORT);
+});
