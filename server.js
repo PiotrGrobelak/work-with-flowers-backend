ff0dbb1560f3f263d66b0dfe8f7e0c82c5c435b2
PiotrGrobelak <grobelak.piotr@gmail.com>
2020-04-26 21:55:51 +0200

    feat: add linter and husky

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,21 +8,19 @@ const mongoose = require('mongoose');
 const PORT = 3000;
 
 mongoose.connect('mongodb://127.0.0.1:27017/flowersDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
-db.once('open', function() {
-    console.log("Connect with flowersDB");
-})
-
+db.once('open', () => {
+  console.log('Connect with flowersDB');
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
 app.listen(PORT, () => {
-    console.log("Server is running on Port: " + PORT);
+  console.log(`Server is running on Port: ${PORT}`);
 });
