27a00844a5522a9a0ad5bec8e848ac77bf816c65
PiotrGrobelak <grobelak.piotr@gmail.com>
2020-04-29 14:38:43 +0200

    feat: add passport config

---
+++ src/config/passport.js
@@ -0,0 +1,49 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const User = require('../models/User');
+
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['acess_token'];
+  }
+  return token;
+};
+
+// authorization
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: process.env.SECRET,
+    },
+    (paylod, done) => {
+      User.findById({ _id: paylod.sub }, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    },
+  ),
+);
+
+// authentication local strategy
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      user.comparePassword(password, done);
+    });
+  }),
+);
